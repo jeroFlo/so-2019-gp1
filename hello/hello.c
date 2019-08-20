0dcee393968227b2cb927ee5905487c2db65cc27
jeroFlo <girasol_gril_flores@hotmail.com>
2019-08-20 01:07:47 +0000

    hello example

---
+++ hello/hello.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+#include "./libhello.h"
+
+int main (int argc,char **argv){
+    printHello(argv[1]);
+    return 0;
+}
---
+++ hello/libhello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "./libhello.h"
+
+void printHello(char *str){
+     printf("Hello world %s\n",str);
+}
