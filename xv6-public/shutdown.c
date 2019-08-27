f8b436d47480897acd26aa0b13889e958113f695
jeroFlo <girasol_gril_flores@hotmail.com>
2019-08-27 01:56:15 +0000

    PRACTICA 1:llamada al sistema

---
+++ xv6-public/shutdown.c
@@ -0,0 +1,8 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[]){
+     shutdown();
+     exit(); //en xv6 en lugar de return 0 se pondra esto para que termine el programa
+}
