947b89a1b5598039e838f941e61a50d273f6bb8b
jeroFlo <girasol_gril_flores@hotmail.com>
2019-09-02 04:54:39 +0000

    Se agrego comando reboot al SO

--- xv6-public/sysproc.c
+++ xv6-public/sysproc.c
@@ -89,3 +89,11 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+int sys_shutdown(void){
+     outw(0x604,0x2000);
+     return 0;
+}
+int sys_reboot(void){
+     outw(0x64,0xFE);
+     return 0;
+}
