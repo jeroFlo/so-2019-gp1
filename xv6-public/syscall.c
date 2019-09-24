10675d49105243527b5c028de8313793d3b8fd8f
jeroFlo <girasol_gril_flores@hotmail.com>
2019-09-24 01:38:43 +0000

    Practica 2: evitar matar init

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -143,7 +143,7 @@ syscall(void)
 
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    cprintf("command called %s -> %d\n",nameCallSystem[num-1],num);
+    //cprintf("command called %s -> %d\n",nameCallSystem[num-1],num);
     curproc->tf->eax = syscalls[num]();
   } else {
     cprintf("%d %s: unknown sys call %d\n",
